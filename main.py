ac3c7b526c9994a6a7db59f5d19d711285443396
frillyguidance23 <cbereznitsky1@gmail.com>
2025-07-19 21:34:04 -0500

    Added formatting and argv optionality

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
-from stats import get_word_count, get_chars
+from stats import get_word_count, get_chars, sort_dict
+import sys
 
 def main():
-    text = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    text = get_book_text(filepath)
     count = get_word_count(text)
-    print(f"{count} words found in the document")
-    print(f"{get_chars(text)}")
+    sorted = sort_dict(get_chars(text))
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for char in sorted:
+        print(f"{char['char']}: {char['count']}")
+    print("============= END ===============")
 
 
 def get_book_text(path_to_file):
--- stats.py
+++ stats.py
@@ -12,3 +12,16 @@ def get_chars(text):
         else:
             chars[char] = 1
     return chars
+
+
+def sort_dict(dict):
+    list_of_new_dicts = []
+    new_dict = {}
+    # create new list containing dictionaries of new key/value pairs char:char and count:count
+    for char, value in dict.items():
+        if char.isalpha():
+            new_dict = {"char": char, "count": value}
+            list_of_new_dicts.append(new_dict)
+    # sort list by count from highest to lowest (requires reverse language)
+    sorted_by_count = sorted(list_of_new_dicts, key=lambda x: x["count"], reverse=True)
+    return sorted_by_count
